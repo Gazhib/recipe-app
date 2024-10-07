6b1352f990176e8086ca8ec9cbe1a451e8ccf073
Gazhib <bvalerianuly@gmail.com>
2024-10-07 16:43:13 +0500

    trying to deploy changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { createHashRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./Pages/Home";
 import "./index.css";
 import RecipesPage from "./Pages/Recipes";
@@ -18,7 +18,7 @@ import AccountPage, {
 export default function App() {
   const queryClient = new QueryClient();
 
-  const router = createHashRouter([
+  const router = createBrowserRouter([
     {
       path: "/",
       element: <RootLayout />,
