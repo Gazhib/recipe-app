429020dba34efd81f44c2c2759fa4d7a4034f737
Gazhib <bvalerianuly@gmail.com>
2024-10-07 17:09:31 +0500

    trying to deploy

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === "production" ? "/recipe-app/" : "/",
+  base: "/recipe-app/",
 });
