a748fe4f6f58e4f2a8b6b613f1258f0c12bda83d
Gazhib <bvalerianuly@gmail.com>
2024-10-07 16:55:57 +0500

    final try

--- src/Pages/Error.jsx
+++ src/Pages/Error.jsx
@@ -3,6 +3,7 @@ import styles from "./Error.module.css";
 export default function ErrorPage({
   error = "We're sorry, but an error occurred while processing your request.",
 }) {
+  console.log("something")
   return (
     <div className={styles.ErrorPage}>
       <h1>Oops! Something went wrong.</h1>
